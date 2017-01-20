25c6e5e095deac421b5404ba3eedfd5af117fb17
Ari Vuollet <goalitium@kapsi.fi>
2017-01-20 15:02:33 +0200

    GameManager changes and PlayerScore - Spawning enemies should not overlap with each other anymore - Initial scoring for enemy kills

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-	public GameObject skeletonObject;
+	public GameObject EnemyObject;
 	public int CurrentRound { get; private set; }
+
+	public List<PlayerScore> Scores { get; private set; }
 	public int EnemyKills { get; private set; }
 
 	private List<SpawnArea> spawnAreas = new List<SpawnArea>();
-	private List<GameObject> enemies = new List<GameObject>();
+	private List<Enemy> enemies = new List<Enemy>();
 		
 	void Start()
 	{
@@ -21,6 +23,9 @@ public class GameManager : MonoBehaviour
 		spawnAreas.AddRange(FindObjectsOfType<SpawnArea>());
 		if (spawnAreas.Count == 0)
 			Debug.LogError("No spawn areas found in the scene");
+
+		if (EnemyObject == null)
+			Debug.LogError("EnemyObject not set");
 	}
 
 	void Update()
@@ -36,6 +41,12 @@ public class GameManager : MonoBehaviour
 			Debug.LogError("GameManager object not found in the scene");
 
 		return instance;
+	}
+
+	private void SpawnPlayer()
+	{
+
+		// update PlayerScore.PlayerObject with newly spawned player character
 	}
 
 	private void SpawnEnemy()
@@ -54,20 +65,54 @@ public class GameManager : MonoBehaviour
 			return;
 		}
 
-		int spawnIndex = UnityEngine.Random.Range(0, spawnAreas.Count);
-		Vector3 startPosition = spawnAreas[spawnIndex].GetRandomPosition();
+		// try to get a random spawn position which is not already occupied by other enemies or players
 
-		GameObject skeleton = (GameObject)Instantiate(skeletonObject, skeletonsGroup);
+		Vector3 startPosition = Vector3.zero;
+		int spawnAttempts = 10;
+		float checkRadius = 2.0f;
+		for (; spawnAttempts > 0; spawnAttempts--)
+		{
+			int spawnIndex = UnityEngine.Random.Range(0, spawnAreas.Count);
+			startPosition = spawnAreas[spawnIndex].GetRandomPosition();
+
+			bool occupied = false;
+			Collider[] colliders = Physics.OverlapSphere(startPosition, checkRadius);
+			for (int i = 0; i < colliders.Length; i++)
+			{
+				Collider col = colliders[i];
+				Transform parent = col.transform.parent;
+				string tag = (parent != null && parent.gameObject != null) ? parent.gameObject.tag : "";
+				if (tag == "Skeleton" || tag == "Player")
+				{
+					occupied = true;
+					break;
+				}
+			}
+
+			if (!occupied)
+				break;
+		}
+		if (spawnAttempts <= 0)
+		{
+			Debug.LogWarning("Unable to spawn enemy, all spawn areas are already occupied");
+			return;
+		}
+
+		GameObject skeleton = Instantiate(EnemyObject, skeletonsGroup);
 		skeleton.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
 		skeleton.transform.position = startPosition;
 
-		enemies.Add(skeleton);
+		Enemy enemy = skeleton.GetComponent<Enemy>();
+		if (enemy != null)
+			enemies.Add(enemy);
 	}
 
 	public void OnRoundStart()
 	{
 		CurrentRound++;
 
+		SpawnEnemy();
+		SpawnEnemy();
 		SpawnEnemy();
 	}
 
@@ -76,9 +121,48 @@ public class GameManager : MonoBehaviour
 
 	}
 		
-	public void OnDeath(GameObject victim, GameObject killer)
+	public void OnDeath(Character victim, Character killer)
 	{
-		// if victim is a skeleton
-		enemies.Remove(victim);
+		Enemy victimEnemy = victim as Enemy;
+		if (victimEnemy != null)
+		{
+			enemies.Remove(victimEnemy);
+
+			// scoring for players
+			Player killerPlayer = killer as Player;
+			if (killerPlayer != null)
+			{
+				for (int i=0; i<Scores.Count; i++)
+				{
+					if (Scores[i].PlayerObject == killerPlayer)
+					{
+						Scores[i].Score += victimEnemy.Score;
+						Scores[i].Kills++;
+					}
+				}
+			}
+		}
+	}
+
+	public int GetScoreForPlayer(Player player)
+	{
+		for (int i=0; i<Scores.Count; i++)
+		{
+			if (Scores[i].PlayerObject == player)
+				return Scores[i].Score;
+		}
+
+		Debug.LogError("Player does not have a score!");
+		return -1;
+	}
+
+	public int GetScoreForPlayerIndex(int index)
+	{
+		// index 0 should be the local player
+		if (index >= 0 && index < Scores.Count)
+			return Scores[index].Score;
+
+		Debug.LogError("Invalid player index " + index.ToString() + ", Player does not have a score!");
+		return -1;
 	}
 }
---
+++ Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScore
+{
+	public string Name;
+	public int Score;
+	public int Kills;
+	public Player PlayerObject;
+}
