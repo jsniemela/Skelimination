0fbdfaea1011a07c32815250fef7309a21c18ea1
Jonimark <joni.pennala@gmail.com>
2017-01-31 21:39:23 +0200

    HealthBar created but not working so far

---
+++ Assets/Scripts/PlayerHealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthBar : MonoBehaviour {
+
+    Image healthbar;
+    Player player;
+
+    // Use this for initialization
+    void Start ()
+    {
+        healthbar = GetComponent<Image>();
+//        var player = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        healthbar.fillAmount = (float)player.Health / (float)player.MaxHealth;
+	}
+}
